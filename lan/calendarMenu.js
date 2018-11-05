f03a57ca8ecb2e38c8150ee9790c675d6c3ed02a
kaiyue <l>
2018-11-05 19:58:22 +0800

    first commit

---
+++ lan/calendarMenu.js
@@ -0,0 +1,67 @@
+// lan/calendarMenu.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    calendarText: "人类不会从历史中吸取太多的教训，而这正是所以历史教训中最为重要的一条。",
+    calendarAuthr:"英国作家阿道司 赫胥黎"
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  }
+})
